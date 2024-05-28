1c99787a2427e92ebf7d278656c3eb64333c40bf
DomenikW <domenik@ronaldwilhlem.de>
2024-05-28 18:27:51 +0200

    SpielendeSpieler, karteSetzen überarbeitet

--- src/main/java/hwr/oop/skat/gruppe2/domain/SpielendeSpieler.java
+++ src/main/java/hwr/oop/skat/gruppe2/domain/SpielendeSpieler.java
@@ -5,63 +5,75 @@ import java.util.List;
 import java.util.Objects;
 
 public class SpielendeSpieler {
-    Spieler spieler;
-    List<Karte> handKarten;
-    List<Karte> gewonneneKarten;
+  Spieler spieler;
+  List<Karte> handKarten;
+  List<Karte> gewonneneKarten;
 
+  public SpielendeSpieler(Spieler spieler) {
+    this.spieler = spieler;
+    this.handKarten = new ArrayList<>();
+    this.gewonneneKarten = new ArrayList<>();
+  }
 
-    public SpielendeSpieler(Spieler spieler) {
-        this.spieler = spieler;
-        this.handKarten = new ArrayList<>();
-        this.gewonneneKarten = new ArrayList<>();
-    }
-
-    public void karteSetzen(Karte karte, KartenStapel kartenStapel) {
-        // Karte auf der Hand?
-        if (!this.getHandKarten().isEmpty()) {
-            // Trumpfkarte / Ungleiche Farbe
-            if (karte.getFarbe().getWert() != kartenStapel.getGelegteKarten().getFirst().getFarbe().getWert()) {
-                for (Karte i : this.getHandKarten()) {
-                    if (i.getFarbe().getWert() == kartenStapel.getGelegteKarten().getFirst().getFarbe().getWert()) {
-                        return;
-                    }
-                }
-                this.getHandKarten().remove(karte);
-                kartenStapel.addGelegteKarte(karte);
-                return;
+  public void karteSetzen(Karte karte, KartenStapel kartenStapel) {
+    // Karte auf der Hand?
+    if (handKarten.contains(karte)) {
+      // Kartenstapel Leer?
+      if (!kartenStapel.getGelegteKarten().isEmpty()) {
+        // Ungleiche Farbe
+        if (karte.getFarbe()
+            != kartenStapel.getGelegteKarten().getFirst().getFarbe()) {
+        // Schauen ob Spieler nicht bedienen kann
+          for (Karte kartensuche : this.getHandKarten()) {
+            if (kartensuche.getFarbe() == kartenStapel.getGelegteKarten().getFirst().getFarbe()) {
+              return;
             }
-            // Gleiche Farbe
-            this.getHandKarten().remove(karte);
-            kartenStapel.addGelegteKarte(karte);
+          this.getHandKarten().remove(karte);
+          kartenStapel.addGelegteKarte(karte);
+          return;
         }
+        // TODO Bube auf Nichttrumph
+        }
+        // TODO Bube gleicher Farbe auf Farbe
+        // Gleiche Farbe
+        this.getHandKarten().remove(karte);
+        kartenStapel.addGelegteKarte(karte);
+        return;
+      }
+      this.getHandKarten().remove(karte);
+      kartenStapel.addGelegteKarte(karte);
     }
+  }
 
-    public void karteAufDieHand(Karte karte) {
-        this.handKarten.add(karte);
-    }
+  public void karteAufDieHand(Karte karte) {
+    this.handKarten.add(karte);
+  }
 
-    public void karteGewonnen(Karte karte) {
-        this.gewonneneKarten.add(karte);
-    }
+  public void karteGewonnen(Karte karte) {
+    this.gewonneneKarten.add(karte);
+  }
 
-    // Getter
-    public List<Karte> getHandKarten() {
-        return this.handKarten;
-    }
-    public List<Karte> getGewonneneKarten() {
-        return this.gewonneneKarten;
-    }
+  // Getter
+  public List<Karte> getHandKarten() {
+    return this.handKarten;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        SpielendeSpieler that = (SpielendeSpieler) o;
-        return Objects.equals(spieler, that.spieler) && Objects.equals(handKarten, that.handKarten) && Objects.equals(gewonneneKarten, that.gewonneneKarten);
-    }
+  public List<Karte> getGewonneneKarten() {
+    return this.gewonneneKarten;
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(spieler, handKarten, gewonneneKarten);
-    }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    SpielendeSpieler that = (SpielendeSpieler) o;
+    return Objects.equals(spieler, that.spieler)
+        && Objects.equals(handKarten, that.handKarten)
+        && Objects.equals(gewonneneKarten, that.gewonneneKarten);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(spieler, handKarten, gewonneneKarten);
+  }
 }
