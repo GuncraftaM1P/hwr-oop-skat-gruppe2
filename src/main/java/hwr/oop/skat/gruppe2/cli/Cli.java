b383d737565fe7af0d24d2878e0d8de093dedc1c
F D <forscher-vt@gmx.de>
2024-05-29 12:07:26 +0200

    stefan does magic

--- src/main/java/hwr/oop/skat/gruppe2/cli/Cli.java
+++ src/main/java/hwr/oop/skat/gruppe2/cli/Cli.java
@@ -1,32 +1,32 @@
-package hwr.oop.skat.gruppe2.cli;
+package hwr.oop.skat.gruppe2.Cli;
 
 import hwr.oop.skat.gruppe2.persistence.LadenUndSpeichern;
 import hwr.oop.skat.gruppe2.domain.Spieler;
 
 import java.io.PrintStream;
 
+
+
 public class Cli {
-  private final PrintStream systemOut;
 
-  public Cli(PrintStream systemOut, String[] args) {
-    this.systemOut = systemOut;
-    argumentAuswerten(args);
-  }
-
-  private void argumentAuswerten(String[] args) {
-    if (args.length >= 2 && args[0].equals("ErstelleNeuenSpieler")) {
-      neuerSpieler(args);
+    private final PrintStream Systemout;
+    public Cli(PrintStream SystemOut,String[] args) {
+        this.Systemout = SystemOut;
+        ArgumentAuswerten(args);
     }
-  }
 
-  private void neuerSpieler(String[] args) {
-    Spieler spieler = new Spieler(args[1]);
-    LadenUndSpeichern ladenUndSpeichern = new LadenUndSpeichern();
-    ladenUndSpeichern.spielerNeuErstellen(spieler);
-    systemOut.println(
-        "Der neue Spieler "
-            + spieler.getName()
-            + " wurde gespeichert. Und hat die UUID:"
-            + spieler.getUuid());
-  }
+    private void ArgumentAuswerten(String[] args) {
+        if(args.length>=2&& args[0].equals("ErstelleNeuenSpieler")){
+            neuerSpieler(args);
+        }
+    }
+
+    private void neuerSpieler(String[] args){
+
+        Spieler spieler = new Spieler(args[1]);
+        LadenUndSpeichern ladenUndSpeichern = new LadenUndSpeichern();
+        ladenUndSpeichern.spielerNeuErstellen(spieler);
+        Systemout.println("Der neue Spieler "+spieler.getName()+" wurde gespeichert. Und hat die UUID:"+spieler.getUuid());
+
+    }
 }
