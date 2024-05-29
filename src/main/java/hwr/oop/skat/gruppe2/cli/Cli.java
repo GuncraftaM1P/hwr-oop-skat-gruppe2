35053a0ff5fae81678964d23246bcf2ad78e93b1
F D <forscher-vt@gmx.de>
2024-05-29 11:40:20 +0200

    sonarLint warnings 2/x

--- src/main/java/hwr/oop/skat/gruppe2/cli/Cli.java
+++ src/main/java/hwr/oop/skat/gruppe2/cli/Cli.java
@@ -5,28 +5,28 @@ import hwr.oop.skat.gruppe2.domain.Spieler;
 
 import java.io.PrintStream;
 
-
-
 public class Cli {
+  private final PrintStream systemOut;
 
-    private final PrintStream systemOut;
-    public Cli(PrintStream systemOut,String[] args) {
-        this.systemOut = systemOut;
-        argumentAuswerten(args);
+  public Cli(PrintStream systemOut, String[] args) {
+    this.systemOut = systemOut;
+    argumentAuswerten(args);
+  }
+
+  private void argumentAuswerten(String[] args) {
+    if (args.length >= 2 && args[0].equals("ErstelleNeuenSpieler")) {
+      neuerSpieler(args);
     }
+  }
 
-    private void argumentAuswerten(String[] args) {
-        if(args.length>=2&& args[0].equals("ErstelleNeuenSpieler")){
-            neuerSpieler(args);
-        }
-    }
-
-    private void neuerSpieler(String[] args){
-
-        Spieler spieler = new Spieler(args[1]);
-        LadenUndSpeichern ladenUndSpeichern = new LadenUndSpeichern();
-        ladenUndSpeichern.spielerNeuErstellen(spieler);
-        systemOut.println("Der neue Spieler "+spieler.getName()+" wurde gespeichert. Und hat die UUID:"+spieler.getUuid());
-
-    }
+  private void neuerSpieler(String[] args) {
+    Spieler spieler = new Spieler(args[1]);
+    LadenUndSpeichern ladenUndSpeichern = new LadenUndSpeichern();
+    ladenUndSpeichern.spielerNeuErstellen(spieler);
+    systemOut.println(
+        "Der neue Spieler "
+            + spieler.getName()
+            + " wurde gespeichert. Und hat die UUID:"
+            + spieler.getUuid());
+  }
 }
